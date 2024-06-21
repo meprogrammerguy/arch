53ca7330e00f0fbe37862c2f6c46284d16982ded
meprogrammerguy <b65535@gmail.com>
2024-06-21 09:34:23 -0500

    update latest setting files

--- sh/backup.sh
+++ sh/backup.sh
@@ -6,7 +6,7 @@ rm -r -f $HOME/decrypt/private/.gocryptfs.reverse.conf
 gocryptfs -q -init -reverse $HOME/decrypt/private --passfile $HOME/.config/mount.conf
 mkdir $HOME/.tmp/crypt
 gocryptfs -reverse $HOME/decrypt/private $HOME/.tmp/crypt -q --passfile $HOME/.config/mount.conf
-cp -a $HOME/.tmp/crypt $HOME/.tmp/backup
+cp -a $HOME/.tmp/crypt/ $HOME/.tmp/backup
 fusermount -u $HOME/.tmp/crypt
 rm -r -f $HOME/.tmp/crypt
 notify-send -i $icon "files encrypted to:" "$HOME/.tmp/backup"
