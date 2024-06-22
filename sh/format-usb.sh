774c1d98a226c6db52b8fd7c16bdbc93129c94f5
meprogrammerguy <b65535@gmail.com>
2024-06-22 12:45:51 -0500

    update latest setting files

--- sh/format-usb.sh
+++ sh/format-usb.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
+#
+# don't use yet, learn more about sfdisk
+# https://superuser.com/questions/332252/how-to-create-and-format-a-partition-using-a-bash-script
+#
 icon="/usr/share/icons/Dracula/24/actions/flag-green.svg"
-if [[ $(lsblk | grep sda1) ]]
-then
-    umount -q -f /dev/sda1
-fi
 if [[ $(lsblk | grep sda1) ]]
 then
     sudo umount -q -f /dev/sda1
 fi
-sudo mkfs.vfat /dev/sda1 -n MOVIE
-umount -q -f /dev/sda1
-notify-send -i $icon "USB format:" "SUCCESS"
+if [[ $(lsblk | grep -L sda1) ]]
+then
+    #sudo dd if=/dev/zero of=/dev/sda1 bs=1M count=14GB status=progress
+    sudo mkfs.vfat -v /dev/sda1 -n MOVIE
+    sudo umount -q -f /dev/sda1
+    notify-send -i $icon "USB format:" "SUCCESS"
+else
+    notify-send -i $icon "USB format:" "NOPE"
+fi
 if [[ $(lsblk | grep sda1) ]]
 then
     sudo umount -q -f /dev/sda1
