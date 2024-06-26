ab002b6a46504de145cacf1afd904f0a77e7c71b
meprogrammerguy <b65535@gmail.com>
2024-06-26 08:30:09 -0500

    update latest setting files

--- sh/conf-to-git.sh
+++ sh/conf-to-git.sh
@@ -13,6 +13,7 @@ cp $args $HOME/.config/sway/dark-wood.jpg $HOME/git/arch/sway/dark-wood.jpg
 cp $args $HOME/.config/sway/definitions.d/* $HOME/git/arch/sway/definitions.d/
 cp -r $args $HOME/.config/sway/scripts/* $HOME/git/arch/sway/scripts/
 cp -r $args $HOME/.config/sway/examples/* $HOME/git/arch/sway/examples/
+cp -r $args $HOME/.config/wofi-wifi-menu/* $HOME/git/arch/wofi-wifi-menu/
 cp $args $HOME/.config/sway/config.d/* $HOME/git/arch/sway/config.d/
 cp $args $HOME/.config/sworkstyle/config.toml $HOME/git/arch/sworkstyle/config.toml
 cp $args $HOME/.config/waybar/* $HOME/git/arch/waybar/
