97fffaa918943cdbbeccf9423433459d4ca96e96
meprogrammerguy <b65535@gmail.com>
2024-07-01 17:49:02 -0500

    update latest setting files

--- .sh/dvd-yad.sh
+++ .sh/dvd-yad.sh
@@ -26,20 +26,30 @@ then
     notify-send -i $icon "$title" "user has cancelled"
     exit 1
 fi
+dvd_dir=$(echo "$the_dvd" | sed 's:/[^/]*$::')
+#if [[ $the_dvd -eq $dvd_dir ]]
+#then
+    #echo "user has not picked a file,  quitting..." >> $log_file
+    #dt=$(date '+%d/%m/%Y %H:%M:%S');
+    #echo "*** end: $dt ***" >> $log_file
+    #notify-send -i $icon "$title" "user has not picked a file"
+    #exit 1
+#fi
 echo " " >> $log_file
 echo "============================================================================================" >> $log_file
 dvd_file=$(echo "$the_dvd" | sed 's|.*/||')
 #echo "/hello/file.txt" | sed 's:/[^/]*$::'
 #dvd_type="${the_dvd##*.}"
 #dvd_type="${filename##*.}"
-dvd_dir=$(echo "$the_dvd" | sed 's:/[^/]*$::')
 dvd_type="${dvd_file##*.}"
-dvd_name="${dvd_file%.*}"
+dvd_name="${dvd_file%.*}.mpg"
 echo " " >> $log_file
+#echo "the full is:      $the_dvd" >> $log_file
 echo "directory is:     $dvd_dir" >> $log_file
 echo "file is:          $dvd_file" >> $log_file 
-echo "name is:          $dvd_name" >> $log_file 
+echo "template is:      xx-$dvd_name" >> $log_file 
 echo "file type is:     $dvd_type" >> $log_file
+echo " " >> $log_file
 echo "============================================================================================" >> $log_file
 
 yad --text-info --text-align=center --title=$title --text="continue?" --fore=green --filename=$log_file
@@ -53,7 +63,7 @@ then
     exit 1
 fi
 q_dvd=$(echo '"'$the_dvd'"')
-q_dir=$(echo '"'$default_dir.convert/xx-temp.mpg'"')
+#q_dir=$(echo '"'$default_dir.convert/xx-temp.mpg'"')
 counter=0
 minutes=0
 #while [ 1 ]
@@ -63,15 +73,16 @@ minutes=0
     minutes=$[minutes - hours * 60 ]
     time_offset=$(echo "$hours:$minutes:0")
     counter=$(printf "%02d" $counter)
-    q_dir=$(echo '"'$default_dir.convert/$counter-$dvd_file.mpg'"')
+    q_dir=$(echo '"'$default_dir.convert/$counter-$dvd_name'"')
     ffmpeg_info=$(echo /usr/bin/ffmpeg -ss "$time_offset" -y -i\
         "$q_dvd" \
         -f dvd -target ntsc-dvd -b:v 5000k -r 30000/1001 -filter:v scale=720:480 -ar 48000 -b:a 384k   -t 0:30:0\
         "$q_dir")
     echo "$ffmpeg_info" > $HOME/.tmp/ffmpeg_info.sh
+    echo "/usr/bin/ffmpeg -i "$q_dir" 2>&1 | grep Duration | cut -d ' ' -f 4 | sed s/,//" >> $HOME/.tmp/ffmpeg_info.sh
     chmod a+x $HOME/.tmp/ffmpeg_info.sh
     cat $HOME/.tmp/ffmpeg_info.sh
-    #$HOME/.tmp/ffmpeg_info.sh
+    $HOME/.tmp/ffmpeg_info.sh
     minutes=$[minutes + 30]
 #done
 dt=$(date '+%d/%m/%Y %H:%M:%S');
